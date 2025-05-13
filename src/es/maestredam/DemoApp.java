0388ed237270ce74d4428733a7cdb25ba7a96b79
javimoli5 <xaviermpmm@gmail.com>
2025-05-13 19:18:00 +0200

    "Commit 2: cambios en el código"
    añado un bucle con objetos

--- src/es/maestredam/DemoApp.java
+++ src/es/maestredam/DemoApp.java
@@ -3,6 +3,17 @@ package es.maestredam;
 public class DemoApp {
 	public static void main(String [] args) {
 		System.out.println("Proyecto Java para Demo");
+
+		Object cosas[] = new Object[3];
+		cosas[0] = new String("Spring");
+		cosas[1] = 7;
+		cosas[2] = 10.005;
+
+		for (Object obj : cosas){
+			System.out.println(obj);
+		}
 	}
+
+
 
 }
